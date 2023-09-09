d1023e42e88ad04cb82f83647d797b900a025869
vhele <vheletiuk17@gmail.com>
2023-09-09 22:37:01 +0300

    hw4

---
+++ lesson3/aditional/main1.js
@@ -0,0 +1,19 @@
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+
+]
+for (let i = 0; i < users.length; i++) {
+    const user = users[i];
+    if (user.status && user.age>30)
+    document.write(`<div class="user">${user.name}</div>`)
+}
